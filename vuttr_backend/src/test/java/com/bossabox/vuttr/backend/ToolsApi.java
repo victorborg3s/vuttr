1f30c275dca4a5e265870b16e85efe35fa176606
Victor Borges <vborges@gmail.com>
2019-11-28 22:54:21 -0300

    Implements "getToolsWithNoParameter" test without relying on database
    createOrUpdateTool test still not working
    
    Removes the "OAuth2Authentication" param on ToolController.save as it is
    not used.

--- vuttr_backend/src/test/java/com/bossabox/vuttr/backend/ToolsApi.java
+++ vuttr_backend/src/test/java/com/bossabox/vuttr/backend/ToolsApi.java
@@ -1,19 +1,27 @@
 package com.bossabox.vuttr.backend;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
@@ -23,6 +31,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import com.bossabox.vuttr.backend.controller.ToolController;
+import com.bossabox.vuttr.backend.model.Tool;
 
 @SpringBootTest
 @ExtendWith({ RestDocumentationExtension.class, SpringExtension.class })
@@ -34,13 +45,36 @@ class ToolsApi {
 	@Autowired
 	private WebApplicationContext context;
 
+	@MockBean
+	private ToolController toolControllerMock;
+
+	private Tool toolSubject;
+	private List<Tool> toolsSubjects;
+
 	@BeforeEach
 	public void setUp(RestDocumentationContextProvider restDocumentation) {
+		this.toolSubject = new Tool();
+		toolSubject.setId(1);
+		toolSubject.setTitle("Notion");
+		toolSubject.setLink("https://notion.so");
+		toolSubject.setDescription(
+				"All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized. ");
+		List<String> tags = new ArrayList<String>();
+		tags.add("organization");
+		tags.add("planning");
+		tags.add("collaboration");
+		tags.add("writing");
+		tags.add("calendar");
+		toolSubject.setTags(tags);
+
+		toolsSubjects = new ArrayList<Tool>();
+		toolsSubjects.add(toolSubject);
+
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(documentationConfiguration(restDocumentation))
 				.build();
 	}
 
-	FieldDescriptor[] tool = new FieldDescriptor[] {
+	FieldDescriptor[] toolDescriptor = new FieldDescriptor[] {
 			fieldWithPath("id").type(JsonFieldType.NUMBER).description("Unique number that itentifies the tool"),
 			fieldWithPath("title").type(JsonFieldType.STRING).description("Tool's title"),
 			fieldWithPath("link").type(JsonFieldType.STRING).description("Website where to find tool's reference"),
@@ -49,24 +83,31 @@ class ToolsApi {
 
 	@Test
 	public void getToolsWithNoParameter() throws Exception {
+		when(toolControllerMock.getAll(null)).thenReturn(toolsSubjects);
+
 		this.mockMvc.perform(get("/tools").accept(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isOk())
-				.andExpect(content().json(
-						"[{\"id\":1,\"title\":\"fastify\",\"link\":\"https://www.fastify.io/\",\"description\":\"Extremely fast and simple, low-overhead web framework for NodeJS. Supports HTTP2.\",\"tags\":[\"web\",\"framework\",\"node\",\"http2\",\"https\",\"localhost\"]},{\"id\":2,\"title\":\"json-server\",\"link\":\"https://github.com/typicode/json-server\",\"description\":\"Fake REST API based on a json schema. Useful for mocking and creating APIs for front-end devs to consume in coding challenges.\",\"tags\":[\"api\",\"json\",\"schema\",\"node\",\"github\",\"rest\"]},{\"id\":4,\"title\":\"Notion\",\"link\":\"https://notion.so\",\"description\":\"All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized. \",\"tags\":[\"organization\",\"planning\",\"collaboration\",\"writing\",\"calendar\"]}]"))
+				.andExpect(content().json("[{id: 1,title: \"Notion\",link: \"https://notion.so\","
+						+ "        description: \"All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized. \",\n"
+						+ "        tags: [\"organization\",\"planning\",\"collaboration\",\"writing\",\"calendar\"]"
+						+ "}]"))
 				.andDo(document("{class-name}/{method-name}",
 						responseFields(fieldWithPath("[]").type(JsonFieldType.ARRAY).description("An array of tools"))
-								.andWithPrefix("[].", tool)));
+								.andWithPrefix("[].", toolDescriptor)));
 	}
 
-//	@Test
-//	public void getTools_WithTagNode_ListTwoRecords() throws Exception {
-//		this.mockMvc
-//				.perform(get("/tools").content("{\"myString\": \"value\"}").characterEncoding("UTF-8")
-//						.accept(MediaType.APPLICATION_JSON))
-//				.andDo(print()).andExpect(status().isOk()).andExpect(content().json("{\"hello\":\"World\"}"))
-//				.andDo(document("index",
-//						requestFields(fieldWithPath("myString").type(JsonFieldType.STRING)
-//								.description("A sample request field")),
-//						responseFields(fieldWithPath("hello").description("The \"Hello, Word\" message"))));
-//	}
+	@Test
+	public void createOrUpdateTool() throws Exception {
+		when(toolControllerMock.save(toolSubject)).thenReturn(toolSubject);
+
+		this.mockMvc.perform(post("/tools").content("{\"id\": 1,\"title\": \"Notion\",\"link\": \"https://notion.so\","
+				+ "\"description\": \"All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized. \",\n"
+				+ "\"tags\": [\"organization\",\"planning\",\"collaboration\",\"writing\",\"calendar\"]}")
+				.contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8"))
+				.andDo(print()).andExpect(status().isCreated())
+				.andExpect(content().json("{\"id\": 1,\"title\": \"Notion\",\"link\": \"https://notion.so\",\"description\": \"All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized. \", \"tags\": [\"organization\",\"planning\",\"collaboration\",\"writing\",\"calendar\"]}"))
+				.andDo(document("{class-name}/{method-name}",
+						requestFields(toolDescriptor),
+						responseFields(toolDescriptor)));
+	}
 
 }
