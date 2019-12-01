72bfe87447fac1537873ac13a128ae2537fee7d6
Victor Borges <vborges@gmail.com>
2019-12-01 00:37:09 -0300

    Corrects OAuth2Server test

--- oauth2server/src/test/java/com/bossabox/vuttr/Oauth2serverApplicationTests.java
+++ oauth2server/src/test/java/com/bossabox/vuttr/Oauth2serverApplicationTests.java
@@ -1,9 +1,19 @@
 package com.bossabox.vuttr;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import com.bossabox.vuttr.oauth2server.Oauth2ServerApplication;
+import com.bossabox.vuttr.oauth2server.config.OAuth2AuthorizationServerConfig;
+import com.bossabox.vuttr.oauth2server.config.ServerWebSecurityConfig;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = { Oauth2ServerApplication.class, OAuth2AuthorizationServerConfig.class,
+		ServerWebSecurityConfig.class })
+@ActiveProfiles("dev")
 class Oauth2serverApplicationTests {
 
 	@Test
