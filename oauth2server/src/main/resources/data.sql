8d051bd15758d716c25b73117415e8accd466a64
Victor Borges <vborges@gmail.com>
2019-12-05 00:23:16 -0300

    Resolves CORS blocking with a new Filter

--- oauth2server/src/main/resources/data.sql
+++ oauth2server/src/main/resources/data.sql
@@ -9,32 +9,28 @@ insert into oauth_client_details(
 	access_token_validity,
 	refresh_token_validity,
 	additional_information,
-	autoapprove
-	) values (
-	'oauth2-jwt-client',
-	'resource-server-rest-api',
-	'$2a$08$qvrzQZ7jJ7oy2p/msL4M0.l83Cd0jNsX6AJUitbgRXGzge4j035ha',
-	'read',
-	'password,authorization_code,refresh_token,client_credentials,implicit',
-	'https://google.com',
-	'USER',
-	10800,
-	2592000,
-	null,
-	null);
-insert into authority(name) values ('ADMIN');
+	autoapprove) 
+select * from (
+	select 'oauth2-jwt-client', 'resource-server-rest-api', '$2a$08$qvrzQZ7jJ7oy2p/msL4M0.l83Cd0jNsX6AJUitbgRXGzge4j035ha',
+		'read', 'password,authorization_code,refresh_token,client_credentials,implicit', 
+		'http://localhost:8080/oauth_callback', 'USER', 
+		10800, 2592000, 0, 0) x 
+	where not exists(select * from oauth_client_details);
+
+insert into authority(name) select * from (
+	select 'ADMIN') x WHERE NOT EXISTS (select * from authority);
+
 insert into principal(
 	account_expired,
 	account_locked,
 	credentials_expired,
 	enabled,
 	password,
-	user_name
-	) values (
-	false,
-	false,
-	false,
-	true,
-	'$2a$08$qvrzQZ7jJ7oy2p/msL4M0.l83Cd0jNsX6AJUitbgRXGzge4j035ha',
-	'admin');
-insert into user_authority(authority_id,user_id) values (1,1);
+	user_name) 
+select * from (select false, false, false, true, '$2a$08$qvrzQZ7jJ7oy2p/msL4M0.l83Cd0jNsX6AJUitbgRXGzge4j035ha', 'admin') x 
+	where not exists(select * from principal);
+
+insert into user_authority(authority_id,user_id) 
+	select * from (select 1, 1) x 
+	WHERE NOT EXISTS (select * from user_authority);
+
